5d404c46a5689fc08f67c303566366190143811d
Yann Plougonven--Lastennet <77398553+Yann-Plougonven@users.noreply.github.com>
2024-12-15 02:03:45 +0100

    Partie émission fonctionnelle

--- prototypes/proto_softphone_P2P.py
+++ prototypes/proto_softphone_P2P.py
@@ -12,8 +12,8 @@ class Softphone():
     CHANNELS:int               # 1:mono 2:stereo
     FREQUENCE:int              # fréquence d'échantillonnage 
     NB_ECHANTILLONS:int        # nombre d'échantillons simultanes
-    PORT_SORTIE:int
-    PORT_ENTREE:int
+    PORT_SORTIE:int            # port de sortie
+    PORT_ENTREE:int            # port d'entrée
     
     # Initialisation des variables statiques
     FORMAT = paInt16
@@ -24,25 +24,48 @@ class Softphone():
     PORT_ENTREE = 5001
     
     def __init__(self, ip_correspondant:str):
-        
         # Déclaration des attributs
-        self.__ip_correspondant = ip_correspondant
+        self.__socket_correspondant:tuple[str, int]
+        self.__socket_local:socket
         self.__fin: bool = False
-        self.__socket_emetteur:socket
         
         # Declaration des attributs audio
-        self.__audio: PyAudio             # connecteur audio
+        self.__audio: PyAudio                    # connecteur audio
         self.__stream_entree:PyAudio.Stream      # flux audio
-        self.__data:bytes     # liste des enregistrements simultanes
+        self.__data:bytes                        # liste des enregistrements simultanés
         
-        # initialisation des attributs réseau
-        self.__socket_emetteur = socket(AF_INET, SOCK_DGRAM)
-        self.__socket_emetteur.bind(("", Softphone.PORT_SORTIE))
-        self.__adr_recepteur = ("192.168.28.87", Softphone.PORT_ENTREE)
-        print("Le serveur UDP a démarré sur le port", Softphone.PORT_SORTIE)
+        # initialisation des attributs réseau du correspondant
+        self.__socket_correspondant = (ip_correspondant, Softphone.PORT_ENTREE)
+        
+        # initialisation des attributs réseau locaux
+        self.__socket_local = socket(AF_INET, SOCK_DGRAM)
+        self.__socket_local.bind(("", Softphone.PORT_SORTIE))
+        print(f"Le serveur UDP a démarré sur le port {Softphone.PORT_SORTIE}.")
         
         # initialisation des attributs audio
         self.__audio = PyAudio()   # Initialisation port audio
         self.__stream_entree = self.__audio.open(format = Softphone.FORMAT, channels = Softphone.CHANNELS,
                                                  rate= Softphone.FREQUENCE, input=True,
                                                  frames_per_buffer = Softphone.NB_ECHANTILLONS)
+        
+        # Lancement de l'enregistrement de la voix
+        print ("Lancement de l'enregistrement de votre voix...")
+        
+        try:
+            while not self.__fin:
+                data = self.__stream_entree.read(2*Softphone.NB_ECHANTILLONS) # lecture des echantillons simultanes
+                self.__socket_local.sendto(data, self.__socket_correspondant)
+                
+        except KeyboardInterrupt:
+            pass
+        
+        finally: # fermeture de la communication
+            self.__audio.close(self.__stream_entree)
+            self.__socket_local.close()
+            # self.__stream_entree.stop_stream()
+            # self.__stream_entree.close()
+            print ("Fin de la communication.")
+
+
+if __name__ == "__main__":
+    softphone:Softphone = Softphone("127.0.0.2")
